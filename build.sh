8f2f9125bbd4215461521da6b45ba13075d15003
demonlord1997 <2273893094@qq.com>
2021-02-23 13:26:38 +0800

    add os choose

--- build.sh
+++ build.sh
@@ -1,9 +1,16 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 CONFIG=$HOME/.config/dashboard-shell
 mkdir $CONFIG -p
-cp folders.ini files.ini config.ini dashboard-shell-run.sh $CONFIG
-sudo cp dashboard-shell /usr/local/bin/dashboard-shell
+cp config.ini dashboard-shell-run.sh $CONFIG
 
+if [ ! -d /usr/local/bin  ]; then
+  sudo mkdir /usr/local/bin
+fi
 
-
+sysOS=`uname -s`
+if [ $sysOS == "Darwin" ];then
+    sudo cp dashboard-shell-mac /usr/local/bin/dashboard-shell
+elif [ $sysOS == "Linux" ];then
+    sudo cp dashboard-shell /usr/local/bin/dashboard-shell
+fi
