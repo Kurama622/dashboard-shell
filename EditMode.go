845ca9d2b8aaee3d5a5a089cab2b3ba99fdcc0ad
demonlord1997 <2273893094@qq.com>
2021-02-28 11:51:21 +0800

    add editmode

---
+++ EditMode.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+    "fmt"
+    "github.com/jroimartin/gocui"
+)
+
+func editMode(g *gocui.Gui, v *gocui.View) error {
+    fmt.Println(editor)
+    err := quit(g, v)
+    if err != nil {
+        return err
+    }
+    return nil
+}
