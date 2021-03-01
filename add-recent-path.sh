4630f53f20089931277dc88f1aab9eb30f0e027f
demonlord1997 <2273893094@qq.com>
2021-03-02 01:17:44 +0800

    fix error

--- add-recent-path.sh
+++ add-recent-path.sh
@@ -1,19 +1,23 @@
 #! /bin/sh
+
 RECDIR=`dirs | head -20`
 DIR_LIST=(${RECDIR// /\\n})
 AddRecFolder=$HOME/.config/dashboard-shell/.AddRecFolder
 RecFolderPath=$HOME/.config/dashboard-shell/RecFolder
+
 if [ ! -f $AddRecFolder ]; then
     touch $AddRecFolder
     existRFP=false
 else
-    sed -i '1!G;h;$!d' $AddRecFolder
+    existRFP=true
 fi
-echo ${DIR_LIST[@]} >> $AddRecFolder
+
+tmp=`cat $AddRecFolder`
+echo ${DIR_LIST[@]} > $AddRecFolder
+if $existRFP; then
+    echo $tmp >> $AddRecFolder
+fi
 awk '!a[$0]++' $AddRecFolder > $RecFolderPath
-if $existRFP ; then
-    sed -i '1!G;h;$!d' $RecFolderPath
-fi
 sed -i '21,$d' $RecFolderPath
 cp $RecFolderPath $AddRecFolder
 
