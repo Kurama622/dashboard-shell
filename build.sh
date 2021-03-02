cdc493aaf72fb9c30689b36c563eb36b410cb22a
demonlord1997 <2273893094@qq.com>
2021-03-02 13:10:42 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -14,3 +14,29 @@ if [ $sysOS == "Darwin" ];then
 elif [ $sysOS == "Linux" ];then
     sudo cp dashboard-shell /usr/local/bin/dashboard-shell
 fi
+
+
+CURSHELL=`echo $SHELL | awk -F "/" '{print $NF}'`
+[ $CURSHELL==zsh ]
+
+if [ $CURSHELL = bash ]; then
+   cp .dashboard-shell.sh $CONFIG/.dashboard-shell.bash
+   echo """
+# dashboard-shell
+
+[ -f $CONFIG/.dashboard-shell.bash ] && source $CONFIG/.dashboard-shell.bash""" >> $HOME/.bashrc
+
+elif [ $CURSHELL = zsh ]; then
+   cp .dashboard-shell.sh $CONFIG/.dashboard-shell.zsh
+   echo """
+# dashboard-shell
+
+[ -f $CONFIG/.dashboard-shell.zsh ] && source $CONFIG/.dashboard-shell.zsh""" >> $HOME/.zshrc
+
+else
+   cp .dashboard-shell.sh $CONFIG/.dashboard-shell.fish
+   echo """
+# dashboard-shell
+
+[ -f $CONFIG/.dashboard-shell.fish ] && source $CONFIG/.dashboard-shell.fish""" >> $HOME/.fishrc
+fi
